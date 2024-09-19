b338dbaba45f89c36a8ec7f05c0bd2fe09358f30
Hidetake Iwata <int128@gmail.com>
2024-09-19 11:01:41 +0900

    Initial import

--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,27 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 
 type Inputs = {
-  name: string
+  configYAML: string
+  image: string
+  ports: string[]
+  environments: string[]
 }
 
-// eslint-disable-next-line @typescript-eslint/require-await
 export const run = async (inputs: Inputs): Promise<void> => {
-  core.info(`my name is ${inputs.name}`)
+  core.info('Starting OpenTelemetry Collector')
+  const otelcolArgs = []
+  const dockerRunArgs = ['-q', '-d', '--name', 'opentelemetry-collector']
+  for (const port of inputs.ports) {
+    dockerRunArgs.push('-p', port)
+  }
+  for (const environment of inputs.environments) {
+    dockerRunArgs.push('-e', environment)
+  }
+  if (inputs.configYAML) {
+    // https://opentelemetry.io/docs/collector/configuration/
+    dockerRunArgs.push('-e', `INLINE_OTELCOL_CONFIG=${inputs.configYAML}`)
+    otelcolArgs.push('--config', 'env:INLINE_OTELCOL_CONFIG')
+  }
+  await exec.exec('docker', ['run', ...dockerRunArgs, inputs.image, ...otelcolArgs])
 }
